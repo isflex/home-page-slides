86ee107fd1159712235a871763cf876ba92bb718
ischerer <hello@flexiness.com>
2023-01-13 11:58:45 +0100

    rework setup:monorepo - c

--- cli.js
+++ cli.js
@@ -8,18 +8,19 @@
 // https://linuxize.com/post/how-to-exclude-files-and-directories-with-rsync/
 
 const sh = require('shell-exec')
-sh(`env-cmd -f ./env/public/.env.${process.env.FLEX_MODE}`)
+sh(`env-cmd -f ./env/public/.env.${process.env.FLEX_MODE} 2>&1`)
 
+console.log(`$FLEX_MODE : ${process.env.FLEX_MODE}`)
 console.log(`$FLEX_MF_HOMEPAGE_ABOUT_SLIDES_PROJECT_DIR : ${process.env.FLEX_MF_HOMEPAGE_ABOUT_SLIDES_PROJECT_DIR}`)
 console.log(`$PROJECT_CWD : ${process.env.PROJECT_CWD}`)
 
 // const args = require('get-them-args')(process.argv.slice(2))
 // console.log(`args for ${process.env.npm_package_name} : ${args}.`)
 
-const dst_directory = `${process.env.PROJECT_CWD}/${process.env.FLEX_MF_HOMEPAGE_ABOUT_SLIDES_PROJECT_DIR}`
+const dst_directory = `${process.env.PROJECT_CWD}/packages/slides`
 
 new Promise(() => {
-  return sh(`rsync -a --exclude-from='./cli-setup-monorepo-exclude-file.txt' ./ ${dst_directory}/ 2>&1`)
+  return sh(`mkdir -p ${dst_directory} && rsync -a --exclude-from='./cli-setup-monorepo-exclude-file.txt' ./ ${dst_directory}/ 2>&1`)
     .then(() => {
       console.log(`Successfully completed 'setup:monorepo' in ${process.env.npm_package_name}.`)
     })
