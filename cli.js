bcf78fd5e02eb1be017d81ca0ab66636f0319e7f
ischerer <hello@flexiness.com>
2023-01-13 20:27:41 +0100

    rework setup:monorepo - t

--- cli.js
+++ cli.js
@@ -12,117 +12,57 @@ const sh = require('shell-exec')
 const parse = require('get-them-args')
 let options = {}
 let method
+let prj_name
 const workDir = path.resolve(__dirname)
 
-// sh(`env-cmd -f ./env/public/.env.${process.env.FLEX_MODE}`)
-// console.log(`$FLEX_MF_HOMEPAGE_ABOUT_SLIDES_PROJECT_DIR : ${process.env.FLEX_MF_HOMEPAGE_ABOUT_SLIDES_PROJECT_DIR}`)
-
-// sh(`set -a`)
-// sh(`. ./env/public/.env.${process.env.FLEX_MODE}`)
-// sh(`set +a`)
-// console.log(`$FLEX_MF_HOMEPAGE_ABOUT_SLIDES_PROJECT_DIR : ${process.env.FLEX_MF_HOMEPAGE_ABOUT_SLIDES_PROJECT_DIR}`)
-
-console.log(`$FLEX_MODE : ${process.env.FLEX_MODE}`)
-console.log(`$PROJECT_CWD : ${process.env.PROJECT_CWD}`)
-console.log("Current working directory : ", workDir)
+// console.log(`$FLEX_MODE : ${process.env.FLEX_MODE}`)
+// console.log(`$PROJECT_CWD : ${process.env.PROJECT_CWD}`)
+// console.log("Current working directory : ", workDir)
 options = parse(process.argv.slice(2))
-console.log(options)
-method = options?.method || 'start'
+// console.log(options)
+method = options?.method || 'install'
 
-if (method === 'start') {
-  console.log('Will launch another day!!')
-  // const scriptCmds = [
-  //   `cd ${workDir}`,
-  //   `yarn install`,
-  //   `yarn run start`
-  // ]
-  
-  // Promise.all(scriptCmds.map(currentCmd => {
-  //   return sh(currentCmd).then((result) => {
-  //     console.log(result)
-  //   })
-  //   .catch((error) => {
-  //     console.log(error)
-  //   })
-  // }))
-  
-} else if (method === 'install' && options?.dir) {
+async function getProjectName() { 
+  return sh(`jq '.name' ${`${workDir}/package.json`}`).then((result) => {
+    const { stdout } = result
+    if (!stdout) return result
+    return stdout.trim()
+  })
+  .catch(() => {
+    Promise.reject(new Error('No prj_name'))
+  })
+}
 
-  // From root run : yarn slides cli --method install --dir packages/slides
+getProjectName()
+  .then((result) => {
+    prj_name = result.replace(/['"]+/g, '')
 
-  const dst_directory = `${process.env.PROJECT_CWD}/${options.dir}`
-  const prj_name = sh(`jq '.name' ${`${workDir}/package.json`}`).then((result) => {
-    console.log(result)
-    return result
+    if (method === 'install' && options?.dir) {
+      const dst_directory = `${process.env.PROJECT_CWD}/${options.dir}`
+      console.log(`Installing ${prj_name} as new workspace into ${process.env.npm_package_name}`)
+      
+      const scriptCmds = [
+        `jq  'del(.bin) | del(.scripts.cli)' ${`${workDir}/package.json`} > "tmp" && mv "tmp" ${`${workDir}/package.json`}`
+        `rsync -a --progress --exclude-from='${`${workDir}/cli-setup-monorepo-exclude-file.txt`}' ${workDir}/ ${dst_directory}/`,
+        `jq --arg dir "${options.dir}" --arg name "${prj_name}" 'del(.dependencies[($name)]) | .resolutions += {($name):("workspace:./"+$dir)} | .workspaces.packages |= (.+ ["./"+$dir] | unique)' ${`${process.env.PROJECT_CWD}/package.json`} > "tmp" && mv "tmp" ${`${process.env.PROJECT_CWD}/package.json`}`
+      ]
+
+      Promise.all(scriptCmds.map(currentCmd => {
+        return sh(currentCmd).then((result) => {
+          console.log(result)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+      }))
+
+    } else {
+      const regex = /^@(.*)\//
+      const short_prj_name = prj_name.replace(regex, '')
+      console.log("From project root run the following cmd :")
+      console.log(`yarn ${short_prj_name} cli --method install --dir path/to/install/workspace`)
+    }
   })
   .catch((error) => {
     console.log(error)
-    return Promise.reject(new Error('No prj_name'))
   })
-
-  const scriptCmds = [
-    `rsync -a --progress --exclude-from='${`${workDir}/cli-setup-monorepo-exclude-file.txt`}' ${workDir}/ ${dst_directory}/`,
-    `jq --arg dir "${options.dir}" --arg name "${prj_name}" '.resolutions += {($name):("workspace:./"+$dir)}' ${`${process.env.PROJECT_CWD}/package.json`} > "tmp" && mv "tmp" ${`${process.env.PROJECT_CWD}/package.json`}`
-    `jq --arg dir "${options.dir}" '.workspaces.packages |= (.+ ["./"+$dir] | unique)' ${`${process.env.PROJECT_CWD}/package.json`} > "tmp" && mv "tmp" ${`${process.env.PROJECT_CWD}/package.json`}`
-  ]
-
-  Promise.all(scriptCmds.map(currentCmd => {
-    return sh(currentCmd).then((result) => {
-      console.log(result)
-    })
-    .catch((error) => {
-      console.log(error)
-    })
-  }))
-
-  // return sh(`rsync -a --progress --exclude-from='${`${workDir}/cli-setup-monorepo-exclude-file.txt`}' ${workDir}/ ${dst_directory}/`).then((result) => {
-  //   console.log(result)
-  // })
-  // .catch((error) => {
-  //   console.log(error)
-  // })
-
-}
-
-// sh(`echo $(rsync --version)`).then((result) => {
-//   console.log(result)
-// })
-// .catch((error) => {
-//   console.log(error)
-// })
-
-// sh(`env-cmd -f ./env/public/.env.${process.env.FLEX_MODE}`).then((result) => {
-//   console.log(`$FLEX_MF_HOMEPAGE_ABOUT_SLIDES_PROJECT_DIR : ${process.env.FLEX_MF_HOMEPAGE_ABOUT_SLIDES_PROJECT_DIR}`)
-//   console.log(result)
-// })
-// .catch((error) => {
-//   console.log(error)
-// })
-
-// const dst_directory = `${process.env.PROJECT_CWD}/packages/slides`
-
-// sh(`rsync -a --exclude-from='./cli-setup-monorepo-exclude-file.txt' ./ ${dst_directory}/`).then((result) => {
-//   console.log(result)
-// })
-// .catch((error) => {
-//   console.log(error)
-// })
-
-// new Promise(() => {
-//   return sh(`env-cmd -f ./env/public/.env.${process.env.FLEX_MODE}`)
-//     .then(res => {
-//       const { stdout } = res
-//       if (!stdout) return res
-
-//       console.log(`$FLEX_MF_HOMEPAGE_ABOUT_SLIDES_PROJECT_DIR : ${process.env.FLEX_MF_HOMEPAGE_ABOUT_SLIDES_PROJECT_DIR}`)
-      
-//       sh(`mkdir -p ${dst_directory}`)
-//       sh(`echo $(rsync --version)`)
-//       sh(`rsync -a --exclude-from='./cli-setup-monorepo-exclude-file.txt' ./ ${dst_directory}/`)
-
-//       console.log(`Successfully completed 'setup:monorepo' in ${process.env.npm_package_name}.`)
-//     })
-//     .catch((error) => {
-//       console.log(`Could not complete 'setup:monorepo' in ${process.env.npm_package_name}. ${error.message}.`)
-//     })
-// })
