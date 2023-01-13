0fb2fc2a6667099dc3ad9be420f0f7a2745e7c66
ischerer <hello@flexiness.com>
2023-01-13 14:52:11 +0100

    rework setup:monorepo - m

--- cli.js
+++ cli.js
@@ -12,6 +12,7 @@ const sh = require('shell-exec')
 const parse = require('get-them-args')
 let options = {}
 let method
+const workDir = path.resolve(__dirname)
 
 // sh(`env-cmd -f ./env/public/.env.${process.env.FLEX_MODE}`)
 // console.log(`$FLEX_MF_HOMEPAGE_ABOUT_SLIDES_PROJECT_DIR : ${process.env.FLEX_MF_HOMEPAGE_ABOUT_SLIDES_PROJECT_DIR}`)
@@ -23,16 +24,22 @@ let method
 
 console.log(`$FLEX_MODE : ${process.env.FLEX_MODE}`)
 console.log(`$PROJECT_CWD : ${process.env.PROJECT_CWD}`)
-console.log("Current working directory : ", path.resolve(__dirname))
+console.log("Current working directory : ", workDir)
 options = parse(process.argv.slice(2))
 console.log(options)
 method = options?.method || 'launch'
 
 if (method === 'launch') {
-  console.log('Will launch another day!!')
+  // console.log('Will launch another day!!')
+  return sh(`yarn launch`).then((result) => {
+    console.log(result)
+  })
+  .catch((error) => {
+    console.log(error)
+  })
 } else if (method === 'install' && options?.dir) {
   const dst_directory = `${process.env.PROJECT_CWD}/${options.dir}`
-  return sh(`rsync -a --exclude-from='${path.resolve(__dirname,'/cli-setup-monorepo-exclude-file.txt')}' ./ ${dst_directory}/`).then((result) => {
+  return sh(`rsync -a --exclude-from='${`${workDir}/cli-setup-monorepo-exclude-file.txt`}' ./ ${dst_directory}/`).then((result) => {
     console.log(result)
   })
   .catch((error) => {
