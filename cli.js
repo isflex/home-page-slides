11c7a00680638ea54acc000d7bbd29682ccff74c
ischerer <hello@flexiness.com>
2023-01-13 11:46:34 +0100

    rework setup:monorepo - b

--- cli.js
+++ cli.js
@@ -10,17 +10,20 @@
 const sh = require('shell-exec')
 sh(`env-cmd -f ./env/public/.env.${process.env.FLEX_MODE}`)
 
-const args = require('get-them-args')(process.argv.slice(2))
-console.log(`args for ${process.env.npm_package_name} : ${args}.`)
+console.log(`$FLEX_MF_HOMEPAGE_ABOUT_SLIDES_PROJECT_DIR : ${process.env.FLEX_MF_HOMEPAGE_ABOUT_SLIDES_PROJECT_DIR}`)
+console.log(`$PROJECT_CWD : ${process.env.PROJECT_CWD}`)
+
+// const args = require('get-them-args')(process.argv.slice(2))
+// console.log(`args for ${process.env.npm_package_name} : ${args}.`)
 
 const dst_directory = `${process.env.PROJECT_CWD}/${process.env.FLEX_MF_HOMEPAGE_ABOUT_SLIDES_PROJECT_DIR}`
 
 new Promise(() => {
-  return sh(`rsync -a --exclude-from='./cli-setup-monorepo-exclude-file.txt' ./ ${dst_directory}/`)
+  return sh(`rsync -a --exclude-from='./cli-setup-monorepo-exclude-file.txt' ./ ${dst_directory}/ 2>&1`)
     .then(() => {
-      console.log(`Successfully completed 'setup:monorepo' for ${process.env.npm_package_name}.`)
+      console.log(`Successfully completed 'setup:monorepo' in ${process.env.npm_package_name}.`)
     })
     .catch((error) => {
-      console.log(`Could complete 'setup:monorepo' for ${process.env.npm_package_name}. ${error.message}.`)
+      console.log(`Could not complete 'setup:monorepo' in ${process.env.npm_package_name}. ${error.message}.`)
     })
 })
